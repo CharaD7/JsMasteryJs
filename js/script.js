3757ec3f60ff803336895acb37012c33c8e6eabb
CharaD7 <jijakahn6@gmail.com>
2024-04-05 09:21:58 +0100

    Objects I completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-let age = null;
-let name;
+const person = {
+  name: 'John',
+  age: 25,
+};
 
-console.log(` Age is ${age} and is of type ${typeof age}`);
-console.log(` Name is ${name} and is of type ${typeof name}`);
+console.log(person)
+console.log(`Person is of type ${typeof person}`);
+console.log(`Age is ${person.age} and is of type ${typeof person.age}`);
+console.log(`Name is ${person.name} and is of type ${typeof person.name}`);
+
+const arr = [1, 2, 3, 4];
+
+console.log("")
+console.log(arr)
+console.log(`arr is ${arr} and is of type ${typeof arr}`);
